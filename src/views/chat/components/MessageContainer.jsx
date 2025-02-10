6d11765d43831af56b72e7b9ea829375abaa390e
Manolis Lykos <lykosmanolis@gmail.com>
2025-02-11 00:23:19 +0200

    Implement auto-scrolling for message container

--- src/views/chat/components/MessageContainer.jsx
+++ src/views/chat/components/MessageContainer.jsx
@@ -1,10 +1,20 @@
 import { CircularProgress, Grid2 } from "@mui/material";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ChatMessage from "./ChatMessage";
 import { useGetUserId } from "../../../hooks/useGetUserId";
 
 function MessageContainer({ messages, isLoading }) {
   const userId = useGetUserId();
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
   return (
     <Grid2
       container
@@ -18,6 +28,10 @@ function MessageContainer({ messages, isLoading }) {
         px: 4,
         py: 2,
         overflow: "auto",
+        backgroundColor: "transparent",
+        backgroundImage: "none",
+        position: "relative",
+        zIndex: 1,
       }}
     >
       {isLoading ? (
@@ -30,24 +44,28 @@ function MessageContainer({ messages, isLoading }) {
           <CircularProgress />
         </Grid2>
       ) : (
-        messages.map((message, index, array) => {
-          const currentMessage = message;
-          const nextMessage = array[index + 1];
-          const isCurrentMessageFromUser = currentMessage.sender_id === userId;
+        <>
+          {messages.map((message, index, array) => {
+            const currentMessage = message;
+            const nextMessage = array[index + 1];
+            const isCurrentMessageFromUser =
+              currentMessage.sender_id === userId;
 
-          const extraSpace =
-            nextMessage &&
-            isCurrentMessageFromUser !== (nextMessage.sender_id === userId);
+            const extraSpace =
+              nextMessage &&
+              isCurrentMessageFromUser !== (nextMessage.sender_id === userId);
 
-          return (
-            <ChatMessage
-              key={`chat-message-${index}`}
-              {...message}
-              sent_by_user={isCurrentMessageFromUser}
-              extraSpace={extraSpace}
-            />
-          );
-        })
+            return (
+              <ChatMessage
+                key={`chat-message-${index}`}
+                {...message}
+                sent_by_user={isCurrentMessageFromUser}
+                extraSpace={extraSpace}
+              />
+            );
+          })}
+          <div ref={messagesEndRef} />
+        </>
       )}
     </Grid2>
   );
