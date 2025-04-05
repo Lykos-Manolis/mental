b6f1bf47a68bcf2b69985822a6659f73ed67bafb
Manolis Lykos <lykosmanolis@gmail.com>
2025-04-05 19:34:08 +0300

    Refactor SVG elements in Home component for visual consistency

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -148,148 +148,96 @@ function Home() {
       <WelcomeModal open={welcomeModalOpen} onClose={handleWelcomeModalClose} />
 
       <svg
-        id="visual"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
         style={{
           position: "fixed",
           top: 0,
-          left: 0,
-          width: "100%",
-          height: "100%",
+          right: 0,
           pointerEvents: "none",
           objectFit: "cover",
-          objectPosition: "bottom",
+          objectPosition: "top",
           overflow: "hidden",
+          width: "400px",
+          height: "900px",
         }}
-        width="100%"
-        height="100%"
-        xmlns="http://www.w3.org/2000/svg"
-        xmlnsXlink="http://www.w3.org/1999/xlink"
-        version="1.1"
       >
-        <defs>
-          <linearGradient id="grad1_0" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#fbae3c" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#fbae3c" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad1_1" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#fbae3c" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#c07c00" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad1_2" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#874d00" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#c07c00" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad1_3" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#874d00" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#552100" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad1_4" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#000000" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#552100" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_0" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#fbae3c" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#fbae3c" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_1" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#c07c00" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#fbae3c" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_2" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#c07c00" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#874d00" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_3" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#552100" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#874d00" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <defs>
-          <linearGradient id="grad2_4" x1="0%" y1="0%" x2="100%" y2="100%">
-            <stop offset="30%" stopColor="#552100" stopOpacity="1"></stop>
-            <stop offset="70%" stopColor="#000000" stopOpacity="1"></stop>
-          </linearGradient>
-        </defs>
-        <g transform="translate(450, 0)">
-          <path
-            id="svg-path-5"
-            d="M0 405C-55.9 402.6 -111.8 400.3 -155 374.2C-198.2 348.1 -228.7 298.3 -256 256C-283.3 213.7 -307.4 178.8 -331.7 137.4C-356 96 -380.5 48 -405 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-4"
-            d="M0 324C-44.7 322.1 -89.4 320.2 -124 299.3C-158.5 278.5 -182.9 238.6 -204.8 204.8C-226.6 170.9 -245.9 143 -265.3 109.9C-284.8 76.8 -304.4 38.4 -324 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-3"
-            d="M0 243C-33.5 241.6 -67.1 240.2 -93 224.5C-118.9 208.9 -137.2 179 -153.6 153.6C-170 128.2 -184.4 107.3 -199 82.4C-213.6 57.6 -228.3 28.8 -243 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-2"
-            d="M0 162C-22.4 161.1 -44.7 160.1 -62 149.7C-79.3 139.2 -91.5 119.3 -102.4 102.4C-113.3 85.5 -122.9 71.5 -132.7 55C-142.4 38.4 -152.2 19.2 -162 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-1"
-            d="M0 81C-11.2 80.5 -22.4 80.1 -31 74.8C-39.6 69.6 -45.7 59.7 -51.2 51.2C-56.7 42.7 -61.5 35.8 -66.3 27.5C-71.2 19.2 -76.1 9.6 -81 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-        </g>
-        <g transform="translate(0, 950)">
-          <path
-            id="svg-path-5"
-            d="M0 -405C58.7 -405.7 117.5 -406.4 155 -374.2C192.5 -342 208.8 -276.8 233.3 -233.3C257.9 -189.9 290.6 -168.1 320.6 -132.8C350.6 -97.5 377.8 -48.8 405 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-4"
-            d="M0 -324C47 -324.6 94 -325.1 124 -299.3C154 -273.6 167.1 -221.5 186.7 -186.7C206.3 -151.9 232.5 -134.5 256.5 -106.2C280.5 -78 302.2 -39 324 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-3"
-            d="M0 -243C35.2 -243.4 70.5 -243.8 93 -224.5C115.5 -205.2 125.3 -166.1 140 -140C154.7 -113.9 174.4 -100.8 192.4 -79.7C210.3 -58.5 226.7 -29.3 243 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-2"
-            d="M0 -162C23.5 -162.3 47 -162.6 62 -149.7C77 -136.8 83.5 -110.7 93.3 -93.3C103.1 -76 116.2 -67.2 128.2 -53.1C140.2 -39 151.1 -19.5 162 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-          <path
-            id="svg-path-1"
-            d="M0 -81C11.7 -81.1 23.5 -81.3 31 -74.8C38.5 -68.4 41.8 -55.4 46.7 -46.7C51.6 -38 58.1 -33.6 64.1 -26.6C70.1 -19.5 75.6 -9.8 81 0L0 0Z"
-            fill={theme.palette.background.default}
-            opacity={0.5}
-          ></path>
-        </g>
+        <path
+          id="svg-path-5"
+          d="M405 405C351.8 399.9 298.5 394.9 250 374.2C201.5 353.5 157.7 317.2 129.9 275.1C102.2 232.9 90.5 185 71.5 138.1C52.5 91.3 26.2 45.7 0 0H405V405Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-4"
+          d="M405 324C362.4 319.9 319.8 315.9 281 299.3C242.2 282.8 207.2 253.8 184.9 220.1C162.7 186.3 153.4 148 138.2 110.5C123 73.1 102 36.5 81 0H405V324Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-3"
+          d="M405 243C373.1 240 341.1 236.9 312 224.5C282.9 212.1 256.6 190.3 240 165C223.3 139.8 216.3 111 204.9 82.9C193.5 54.8 177.7 27.4 162 0H405V243Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-2"
+          d="M405 162C383.7 160 362.4 157.9 343 149.7C323.6 141.4 306.1 126.9 295 110C283.9 93.2 279.2 74 271.6 55.3C264 36.5 253.5 18.3 243 0H405V162Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-1"
+          d="M405 81C394.4 80 383.7 79 374 74.8C364.3 70.7 355.5 63.4 350 55C344.4 46.6 342.1 37 338.3 27.6C334.5 18.3 329.2 9.1 324 0H405V81Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+      </svg>
+      <svg
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        style={{
+          position: "fixed",
+          bottom: 0,
+          left: 0,
+          pointerEvents: "none",
+          objectFit: "cover",
+          objectPosition: "left",
+          overflow: "hidden",
+          width: "100%",
+          height: "400px",
+        }}
+      >
+        <path
+          id="svg-path-5"
+          d="M0 0C42.7 30.1 85.3 60.2 136.6 75.2C187.9 90.2 247.8 90.2 286.4 118.6C325 147.1 342.2 204 358.5 256.5C374.7 309 389.8 357 405 405H0V0Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-4"
+          d="M0 81C34.1 105.1 68.3 129.1 109.3 141.1C150.3 153.2 198.2 153.1 229.1 175.9C260 198.7 273.8 244.2 286.8 286.2C299.7 328.2 311.9 366.6 324 405H0V81Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-3"
+          d="M0 162C25.6 180 51.2 198.1 82 207.1C112.7 216.1 148.7 216.1 171.8 233.2C195 250.2 205.3 284.4 215.1 315.9C224.8 347.4 233.9 376.2 243 405H0V162Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-2"
+          d="M0 243C17.1 255 34.1 267.1 54.6 273.1C75.2 279.1 99.1 279.1 114.6 290.4C130 301.8 136.9 324.6 143.4 345.6C149.9 366.6 155.9 385.8 162 405H0V243Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
+        <path
+          id="svg-path-1"
+          d="M0 324C8.5 330 17.1 336 27.3 339C37.6 342 49.6 342 57.3 347.7C65 353.4 68.4 364.8 71.7 375.3C74.9 385.8 78 395.4 81 405H0V324Z"
+          fill={theme.palette.background.default}
+          opacity={0.5}
+        />
       </svg>
     </Grid2>
   );
